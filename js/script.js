f8314441caf2cfe5d68435f792d0428ca211593d
RTO-Juan <jperezchica@hawk.iit.edu>
2025-04-08 23:50:41 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ function init(){
   
   
   function showMeText(){
-    var textbox = document.getElementById('entryinput');
+    var textbox = document.getElementById('entryinput
+    document.getElementById('textoutput').innerHTML = textbox.value;
  
 
 window.addEventListener('load', init);
