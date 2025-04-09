e3eb882e989d1deae3156fabc4d7c5805bcd6123
RTO-Juan <jperezchica@hawk.iit.edu>
2025-04-08 23:49:40 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 function init(){
   var button = document.getElementById('entrybutton');
+  
+  
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
  
-
-
-
-
-
-
 
 window.addEventListener('load', init);
